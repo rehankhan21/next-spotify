aa527047788303fbc7a7bf8f9df241f3b0abce9b
rehankhan21 <narutop224@gmail.com>
2023-07-05 03:13:49 -0400

    added search content and input using query string

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,5 +1,39 @@
+"use client";
+
+// qs helps us stringify urls
+import qs from "query-string";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
+import useDebounce from "@/hooks/useDebounce";
+
+import Input from "./Input";
+
 const SearchInput = () => {
-  return <div>search input</div>;
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debounceValue = useDebounce<string>(value, 500);
+
+  useEffect(() => {
+    const query = {
+      title: debounceValue,
+    };
+
+    const url = qs.stringifyUrl({
+      url: "/search",
+      query: query,
+    });
+
+    router.push(url);
+  }, [debounceValue, router]);
+
+  return (
+    <Input
+      placeholder=" What do you want to listen to?"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
 };
 
 export default SearchInput;
