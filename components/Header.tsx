34ebb14e89f31ad2b5d612be6d55fcc12d4e1544
rehankhan21 <narutop224@gmail.com>
2023-07-05 02:55:36 -0400

    removed borders

--- components/Header.tsx
+++ components/Header.tsx
@@ -40,12 +40,12 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
   return (
     <div
       className={twMerge(
-        ` h-fit bg-gradient-to-b from-emerald-800 p-6 border`,
+        ` h-fit bg-gradient-to-b from-emerald-800 p-6`,
         className
       )}
     >
-      <div className=" w-full mb-4 flex items-center justify-between border">
-        <div className=" hidden md:flex gap-x-2 items-center border">
+      <div className=" w-full mb-4 flex items-center justify-between">
+        <div className=" hidden md:flex gap-x-2 items-center">
           <button
             onClick={() => router.back()}
             className=" rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
--- components/Library.tsx
+++ components/Library.tsx
@@ -29,7 +29,7 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
   };
 
   return (
-    <div className=" flex flex-col border border-green-200">
+    <div className=" flex flex-col">
       <div className=" flex items-center justify-between px-5 pt-4">
         <div className=" inline-flex items-center gap-x-2">
           <TbPlaylist className=" text-neutral-400" size={26} />
