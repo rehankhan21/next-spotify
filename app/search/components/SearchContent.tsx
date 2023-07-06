3e3c1aa86acacfa6adf3c146f94d178966f1f1b8
rehankhan21 <narutop224@gmail.com>
2023-07-06 03:07:03 -0400

    added liked button and functionality

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
+import LikeButton from "@/components/LikeButton";
+import MediaItem from "@/components/MediaItem";
 
 interface SearchContentProps {
   songs: Song[];
@@ -23,7 +24,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
           <div className=" flex-1">
             <MediaItem onClick={() => {}} data={song} />
           </div>
-          {/* TODO: Add like button here */}
+          <LikeButton songId={song.id} />
         </div>
       ))}
     </div>
